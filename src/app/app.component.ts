ccb054c721d971811997096f3891881e68b9cbb8
sowmyasri9 <sowmyasri23164@gmail.com>
2021-05-29 23:01:13 +0530

    Todo app-Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todo-app';
+  strike = false;
+  tasks = [];
+  todoForm = new FormGroup({
+    todoTask: new FormControl('')
+  });
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    const x = this.todoForm.get('todoTask').value;
+    if (x) {
+      this.tasks.push(x);
+    }
+    this.todoForm.get('todoTask').setValue('');
+  }
+
+  onDelete(task) {
+    const index = this.tasks.indexOf(task);
+    if (index > -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
 }
